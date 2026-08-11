6a4ab7400861c0d5ad6d9adf30de859d119326e9
duckittyr-hash <duckittyr@gmail.com>
2026-08-11 20:06:42 +0200

    Update ejercicio0.py

--- ejercicio0.py
+++ ejercicio0.py
@@ -1,3 +1,40 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    # Joined at can be None in very bizarre cases so just handle that as well
+    if member.joined_at is None:
+        await ctx.send(f'{member} has no join date.')
+    else:
+        await ctx.send(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+bot.run("PUT YOUR TOKEN HERE")
+
+
+
+
+
+
 meme_dict = {
     "cringe": "Algo raro ",
     "lol":"Respuesta a algo gracioso",
